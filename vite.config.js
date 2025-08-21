c950f6502fdcd66a532a6d81c2ad45ed4a40d7a0
bardoliwala07 <bardoliwala07@gmail.com>
2025-08-21 18:00:21 +0530

    fix vercel

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
-  resolve: {
-    alias: {
-      fabric: path.resolve(__dirname, "node_modules/fabric/dist/fabric.js"),
-    },
-  },
+  base: "./",
+ 
 })
